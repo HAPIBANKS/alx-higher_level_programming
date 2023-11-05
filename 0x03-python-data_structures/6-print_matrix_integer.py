00894d2298d3d583acca4db0a35a008d10fe0230
HAPIBANKS <posiayo1@gmail.com>
2023-11-05 14:48:06 +0100

    divisible by 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end="\t")
+            print("{:d}".format(i), end="")
+            if i is not row[len(row) - 1]:
+                print(" ", end="")
         print()
