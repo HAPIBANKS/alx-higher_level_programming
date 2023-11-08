b3ca7515a7f027bfe4ae6b6e6670844e318375e6
HAPIBANKS <posiayo1@gmail.com>
2023-11-08 18:55:29 +0000

    squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    square_matrix = []
+    for row in matrix:
+        new_row = []
+        for i in row:
+            new_row.append(i ** 2)
+        square_matrix.append(new_row)
+    
+    return square_matrix
