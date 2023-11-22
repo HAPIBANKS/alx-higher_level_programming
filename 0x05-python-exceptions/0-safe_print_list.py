d8231d3ea0a6e2272993603789fd61a8888993c7
HAPIBANKS <posiayo1@gmail.com>
2023-11-22 21:13:27 +0100

    print safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list, x=0):
             if j == x:
                 break
         print("")
+        return j
     except IndexError:
-        pass
-    return j
+        return
