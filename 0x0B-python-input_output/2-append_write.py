e3490e1d6a1ab1bd1a5d7ee10730e2dc7a048620
HAPIBANKS <posiayo1@gmail.com>
2023-12-10 19:27:23 +0100

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Append to a file
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding=utf-8) as file:
+        append_file = file.append(text)
+        return len(append_file)
