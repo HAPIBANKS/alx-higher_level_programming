3624c2cf1d1249c5d75fbdba8bb3bb493438c123
HAPIBANKS <posiayo1@gmail.com>
2023-12-10 14:42:23 +0100

    read and print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,11 +10,6 @@ def read_file(filename=""):
     Read a text file and print it to standard output
     """
 
-    try:
-        with open(filename, 'r', encoding="utf-8") as file:
-            
-            file_content = file.read()
-            print(file_content)
-    
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open(filename, 'r', encoding="utf-8") as file:        
+        file_content = file.read()
+        print(file_content)
