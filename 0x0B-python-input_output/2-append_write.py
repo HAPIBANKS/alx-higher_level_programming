e1a2138072a851b0477d0f2b4bdf6511dcf44e93
HAPIBANKS <posiayo1@gmail.com>
2023-12-10 19:32:05 +0100

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ Append to a file
 
 def append_write(filename="", text=""):
     """open the file and append"""
-    with open(filename, 'a', encoding=utf-8) as file:
+    with open(filename, 'a', encoding="utf-8") as file:
         append_file = file.append(text)
         return len(append_file)
