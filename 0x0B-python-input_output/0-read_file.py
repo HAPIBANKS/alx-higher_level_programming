9ac6f25ab1b29ba1add6cf8cfc7e22ff30207035
HAPIBANKS <posiayo1@gmail.com>
2023-12-10 14:16:05 +0100

    read and print

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,6 @@ def read_file(filename=""):
     """
     read a text file and print it to standard output
     """
-
-
-
     with open(filename, 'r', encodind="utf-8") as file:
         file_content = file.read()
         print(file_content)
