d6439feee003ad78c83cf11bff775dd334e57523
HAPIBANKS <posiayo1@gmail.com>
2023-12-11 15:07:51 +0100

    add args

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,8 +14,11 @@ import json
 def add_arguments_to_list_and_save():
     """add all arguments"""
     try:
+        """load fromjson file"""
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
+        """create file if it doesn't exist"""
         items = []
+    """add arguments to items"""
     items.extend(sys.argv[1:])
     save_to_json_file("add_item.json", items)
