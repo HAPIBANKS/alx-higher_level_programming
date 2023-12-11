e168ebf2bf6d531d7f3672ed6266fa27b1401c9f
HAPIBANKS <posiayo1@gmail.com>
2023-12-11 15:26:07 +0100

    add args

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,22 +17,18 @@ Dependencies:
 Note:
     If 'add_item.json' doesn't exist, it will be created, and the command-line
     arguments will be added to an empty list.
-
 """
+
 
 import sys
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
 try:
-    # Load existing data from 'add_item.json' or initialize an empty list
     items = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    # Create the file if it doesn't exist
     items = []
 
-# Add command-line arguments to the list
 items.extend(sys.argv[1:])
 
-# Save the updated list to 'add_item.json'
 save_to_json_file("add_item.json", items)
