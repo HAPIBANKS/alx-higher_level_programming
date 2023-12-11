60d7a41757ee67060a0c19cc26db0c90237328fb
HAPIBANKS <posiayo1@gmail.com>
2023-12-11 06:42:56 +0100

    load jsonfile

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+create an object from a JSON file
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """deserialize the json file"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        object_file = json.loads(filename)
