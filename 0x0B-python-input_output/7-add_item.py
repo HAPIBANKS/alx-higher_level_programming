80e067b8e17110e313d29f856159df2cf33e7804
HAPIBANKS <posiayo1@gmail.com>
2023-12-11 15:02:01 +0100

    add all arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,12 +10,11 @@ from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 import json
 
-
-try:
+def add_arguments_to_list_and_save():
     """ add all arguments"""
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
-
-items.extend(sys.argv[1:])
-save_to_json_file("add_item.json", items)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file("add_item.json", items)
