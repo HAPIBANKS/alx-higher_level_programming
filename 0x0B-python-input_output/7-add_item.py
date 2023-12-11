8393c9df17f0cd0e44968776a3d5e504c966085e
HAPIBANKS <posiayo1@gmail.com>
2023-12-11 15:03:54 +0100

    add all arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,9 @@ from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 import json
 
+
 def add_arguments_to_list_and_save():
-    """ add all arguments"""
+    """add all arguments"""
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
