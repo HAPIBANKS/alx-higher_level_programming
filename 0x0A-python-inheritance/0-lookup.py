c86f7199a7e6907288879d8c0a6916af55d7f137
HAPIBANKS <posiayo1@gmail.com>
2023-12-27 07:20:50 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Retruns the list of attribute and methods of an object
+"""
+
+
+def lookup(obj):
+    """get the attribute"""
+    attributes = []
+
+    for key in dir(obj):
+        value = getattr(obj, key)
+    if callable(value):
+        attributes.append(f"{key}()")
+    else:
+        attributes.append(key)
+
+    return attributes
