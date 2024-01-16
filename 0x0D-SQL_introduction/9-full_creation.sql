c2d5beed24fb124be9eddf700f42d983010e101e
HAPIBANKS <posiayo1@gmail.com>
2024-01-16 19:18:06 +0100

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- creates a table second_table
+CREATE table IF NOT EXISTS second_table ('id' INT, 'name' VARCHAR(256), 'score' INT);
+INSERT INTO second_table ('id', 'name', 'score') VALUES ((1, 'John', 10), (2, 'Alex', 3), (4, 'George', 8));
