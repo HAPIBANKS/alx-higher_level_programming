e616ef6b27a0e9cd8d846636419595031966fb8c
HAPIBANKS <posiayo1@gmail.com>
2024-01-17 16:50:28 +0100

    force name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- creates the tale force_name
-CREATE TABLE IF NOT EXISTS force_name('id' INT, 'name' VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
