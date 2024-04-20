3d315c2466c8b0442d9f783e2a973ee9e3c819cd
HAPIBANKS <posiayo1@gmail.com>
2024-04-20 17:57:32 +0100

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,20 @@
-#!/usr/bin/pyhton3
-"""Filter states that start with N from a database
-hbtn_0e_0usa using the module MySQLdb"""
-
+#!/usr/bin/python3
+"""list all states with name starting with `N` from
+database `hbtn_0e_0_usa`
+"""
 
 if __name__ == "__main__":
     import MySQLdb
     import sys
-
     db = MySQLdb.connect(user=sys.argv[1],
-                         passwrd=sys.argv[2],
+                         passwd=sys.argv[2],
                          database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    for state in cur.fetchall:
+    cur.execute("SELECT * FROM states "
+                "WHERE name LIKE 'N%' "
+                "COLLATE 'latin1_general_cs' "
+                "ORDER BY id ASC")
+    for state in cur.fetchall():
         print(state)
     cur.close()
     db.close()
