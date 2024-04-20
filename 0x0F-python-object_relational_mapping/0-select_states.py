9bf01c35be0356a5ca9e3b857470088d5dcebfe6
HAPIBANKS <posiayo1@gmail.com>
2024-04-20 16:24:17 +0100

    select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""Script to select all the states from a table"""
-
-
-import MYSQLdb
-import sys
-
-
-def list_states{username, password, database):
-
-    db = MySQLdb.connect(host="localhost",
-                         user=username,
-                         passwd=password,
-                         db=database,
-                         port=3308)
-    cur = db.cursor()
-    cur.execute("SELECT *FROM states ORDER BY id")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-
-    cur.close()
-    db.close()
+"""script to select all staes from sql table"""
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for state in cur.fetchall():
+        print(state)
+    cur.close()
+    db.close()
