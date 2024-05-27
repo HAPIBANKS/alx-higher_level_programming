b68e8e9a549be413e1e893215be2fd3ad95a38ca
HAPIBANKS <posiayo1@gmail.com>
2024-05-27 16:41:12 +0100

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,6 +4,5 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
-    }
-    else console.log(data);
-    });
+  } else console.log(data);
+});
